3efae3ab1aa22b55dd0ac7b311297a81e0483082
aashnaregmi <regmiashna25@gmail.com>
2026-02-20 23:26:08 +0545

    add Home page with hero section and buttons

--- frontend/5to-do-app/src/ pages/Home.jsx
+++ frontend/5to-do-app/src/ pages/Home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Hi this is Home</div>;
+  return (
+    <div>
+      <div className="home-container">
+        <div className="hero-section">
+          <h1>Plan Your Day. Own Your Life.</h1>
+          <p>
+            Stay productive, organized, and focused with your personal task
+            planner. Write it down. Get it done.
+          </p>
+
+          <div className="home-buttons">
+            <Link to="/signup">
+              <button className="primary-btn">Get Started</button>
+            </Link>
+
+            <Link to="/login">
+              <button className="secondary-btn">Learn More</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
