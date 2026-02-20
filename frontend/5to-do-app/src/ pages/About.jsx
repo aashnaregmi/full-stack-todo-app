a790d14d8a379b67ddce4acea2d49f77e74feaee
aashnaregmi <regmiashna25@gmail.com>
2026-02-20 23:50:52 +0545

    add trial About page and about.css

--- frontend/5to-do-app/src/ pages/About.jsx
+++ frontend/5to-do-app/src/ pages/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./About.css";
+import "./Home.css";
 
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <div className="about-container container">
+      <div className="about-hero">
+        <p>
+          Welcome to <strong>Planner</strong> — your digital sidekick for
+          tackling life one task at a time! Whether it’s school, work, or just
+          remembering to feed your plants, we’ve got your back. Add your tasks,
+          check them off, and watch your day transform from chaos to
+          accomplishment. Productivity has never been this fun, simple, and
+          satisfying!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
