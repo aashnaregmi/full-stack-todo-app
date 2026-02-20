3dab51382656a9d838fd15b61b0e6f8386a19fb0
aashnaregmi <regmiashna25@gmail.com>
2026-02-20 22:59:48 +0545

    updated navbar with links and login button

--- frontend/5to-do-app/src/ pages/Navbar.jsx
+++ frontend/5to-do-app/src/ pages/Navbar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 const Navbar = () => {
-  return <div className="nav-div">This is nav</div>;
+  return (
+    <div className="nav-div">
+      <div className="logo">
+        <h1>Planner</h1>
+      </div>
+
+      <div className="nav-titles">
+        <NavLink to="/">
+          <div className="navlinks">Home</div>
+        </NavLink>
+        <NavLink to="/about">
+          <div className="navlinks">About Us</div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="navlinks">Resources</div>
+        </NavLink>
+
+        <NavLink to="/">
+          <div className="navlinks"> Contact</div>
+        </NavLink>
+        {/* <NavLink to="/todo">
+          {" "}
+          <li>Todo</li>
+        </NavLink> */}
+
+        <button> Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
