d7b22283c2c3e7ecd99a5028805ecfe2daed292e
kushal jirel <kushaljirel12@gmail.com>
2026-02-21 21:46:58 +0545

    make access of environment variables using dotenv and exporting it from env.js and create database connection from connectDB.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
-import express from "express"
+import express from "express";
 
-import { connectDB } from "./db/connectDB.js"
+import {PORT} from "./config/env.js";
+import { connectDB } from "./db/connectDB.js";
 
-const app = express()
-const PORT = process.env.PORT || 5000
-await connectDB()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!")
-})
+  res.send("Hello, World!");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`)
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running in http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error starting Database:", error.message);
+    process.exit(1)
+  }
+};
+
+startServer();
