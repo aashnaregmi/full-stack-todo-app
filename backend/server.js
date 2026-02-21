f60813dbdeb857de4f12c0f101b450149cc5cd04
kushal jirel <kushaljirel12@gmail.com>
2026-02-21 08:29:13 +0545

    create mongodb database connection function in connectDB.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express"
 
+import { connectDB } from "./db/connectDB.js"
+
 const app = express()
 const PORT = process.env.PORT || 5000
-
+await connectDB()
 app.use(express.json())
 
 app.get("/", (req, res) => {
