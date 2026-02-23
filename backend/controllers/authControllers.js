a594d529eac26d96b001a03ac9b6ecfeb55c7877
kushal jirel <kushaljirel12@gmail.com>
2026-02-23 20:27:05 +0545

    login logic initallly added

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -45,3 +45,51 @@ export const signup = async (req, res) => {
         })
     }
 }
+
+export const login = async () => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const userExists = await User.findOne({email})
+        if(userExists){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({username, email, password: hashedPassword})
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "User Signup failed"
+            })
+        }
+
+        return res.status(201).json({
+            success: true,
+            message: "User Signup successful",
+            data: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            msg: error.message
+        })
+    }
+}
+
+
+
