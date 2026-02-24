af7b9003dd253deddac1f22a90dd71c7b883409c
kushal jirel <kushaljirel12@gmail.com>
2026-02-24 18:34:36 +0545

    create login logic in authController.js and added route /login

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,16 +3,16 @@ import User from "../models/user.js"
 
 export const signup = async (req, res) => {
     try {
-        const {username, email, password} = req.body;
-        if(!username || !email || !password){
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
             return res.status(400).json({
                 success: false,
                 message: "All fields are required"
             })
         }
 
-        const userExists = await User.findOne({email})
-        if(userExists){
+        const userExists = await User.findOne({ email })
+        if (userExists) {
             return res.status(400).json({
                 success: false,
                 message: "User already exists"
@@ -20,8 +20,8 @@ export const signup = async (req, res) => {
         }
 
         const hashedPassword = await bcrypt.hash(password, 10)
-        const user = await User.create({username, email, password: hashedPassword})
-        if(!user){
+        const user = await User.create({ username, email, password: hashedPassword })
+        if (!user) {
             return res.status(400).json({
                 success: false,
                 message: "User Signup failed"
@@ -35,6 +35,45 @@ export const signup = async (req, res) => {
                 id: user._id,
                 username: user.username,
                 email: user.email
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            msg: error.message
+        })
+    }
+}
+
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const existingUser = await User.findOne({ email })
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const passwordMatch = await bcrypt.compare(password, hashedPassword)
+        if (!existingUser || !passwordMatch) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credentials."
+            })
+        }
+
+        return res.status(201).json({
+            success: true,
+            message: "User login successful",
+            data: {
+                id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email
             }
         })
     } catch (error) {
